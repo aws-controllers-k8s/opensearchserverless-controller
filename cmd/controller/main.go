53a0b7f3891f6beeef921f78605827f2b19a6588
michaelhtm <98621731+michaelhtm@users.noreply.github.com>
2025-05-28 13:46:14 -0700

    feat: SecurityPolicy resource

--- cmd/controller/main.go
+++ cmd/controller/main.go
@@ -39,6 +39,8 @@ import (
 	svctypes "github.com/aws-controllers-k8s/opensearchserverless-controller/apis/v1alpha1"
 	svcresource "github.com/aws-controllers-k8s/opensearchserverless-controller/pkg/resource"
 
+	_ "github.com/aws-controllers-k8s/opensearchserverless-controller/pkg/resource/security_policy"
+
 	"github.com/aws-controllers-k8s/opensearchserverless-controller/pkg/version"
 )
 
