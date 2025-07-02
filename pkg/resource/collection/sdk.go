accabd4375206de74b23a183bc7ef3381e9299e4
michaelhtm <98621731+michaelhtm@users.noreply.github.com>
2025-07-02 15:40:03 -0700

    create securityPolicy for collection test

--- pkg/resource/collection/sdk.go
+++ pkg/resource/collection/sdk.go
@@ -151,7 +151,7 @@ func (rm *resourceManager) sdkFind(
 	}
 
 	rm.setStatusDefaults(ko)
-	r.ko.Spec.Tags, err = getTags(ctx, string(*ko.Status.ACKResourceMetadata.ARN), rm.sdkapi, rm.metrics)
+	ko.Spec.Tags, err = getTags(ctx, string(*ko.Status.ACKResourceMetadata.ARN), rm.sdkapi, rm.metrics)
 	if err != nil {
 		return &resource{ko}, err
 	}
--- templates/hooks/collection/sdk_read_many_post_set_output.go.tpl
+++ templates/hooks/collection/sdk_read_many_post_set_output.go.tpl
@@ -1,4 +1,4 @@
-	r.ko.Spec.Tags, err = getTags(ctx, string(*ko.Status.ACKResourceMetadata.ARN), rm.sdkapi, rm.metrics)
+	ko.Spec.Tags, err = getTags(ctx, string(*ko.Status.ACKResourceMetadata.ARN), rm.sdkapi, rm.metrics)
 	if err != nil {
 		return &resource{ko}, err
 	}
