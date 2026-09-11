45c7ed92b4605898d032b51be3d6e3a531f8f154
knottnt <trevor.knottnerus@gmail.com>
2026-09-11 12:58:38 -0700

    Rebuild with go_tag aware adoption-fields
    - Re-generate to resolve type_ being used as an adoption field in SecurityPolicy and AccessPolicy
    - Add adoption hooks to provide backwards compatibility for SecurityPolicy

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -27,6 +27,6 @@ var (
 // that generated this controller's code. They are baked in at code-generation
 // time and are immutable for the life of the generated code.
 const (
-	ACKGenerateVersion   = "v0.63.0-8-g3da9ce2"
-	ACKGenerateBuildDate = "2026-09-10T18:01:19Z"
+	ACKGenerateVersion   = "v0.63.0-9-gabb3faf-dirty"
+	ACKGenerateBuildDate = "2026-09-11T18:46:26Z"
 )
